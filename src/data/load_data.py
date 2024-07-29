e41a4aea33fd669bb3e2517a04a45678eb171bc5
Timothy <tianyu.ma2001@foxmail.com>
2024-07-29 10:14:13 +0100

    add label noise and test the robustness of the model

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,25 +1,18 @@
 import torch
 from torch.utils.data import Dataset
+import numpy as np
 import pandas as pd
-
 
 class HARDataSet(Dataset):
     def __init__(self, X_file, y_file):
-        self.X = (
-            pd.read_csv(X_file, header=None, sep=r"\s+").to_numpy().astype("float32")
-        )
-        self.y = (
-            pd.read_csv(y_file, header=None, sep=r"\s+")
-            .to_numpy()
-            .squeeze()
-            .astype("long")
-            - 1
-        )
+        X = pd.read_csv(X_file, header=None, sep=r"\s+").to_numpy()
+        y = pd.read_csv(y_file, header=None, sep=r"\s+").to_numpy().squeeze() - 1
+
+        self.X = torch.tensor(X, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.long)
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, idx):
-        return torch.tensor(self.X[idx], dtype=torch.float32), torch.tensor(
-            self.y[idx], dtype=torch.long
-        )
+        return self.X[idx], self.y[idx]
